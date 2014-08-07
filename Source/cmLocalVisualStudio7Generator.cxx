19880cc55d5913c53692b78ca53bbd15360cef50
Brad King <brad.king@kitware.com>
2014-08-07 15:20:49 -0400

    VS: Add MASM support to VS 8 and 9 (#8170, #14984)

--- Source/cmLocalVisualStudio7Generator.cxx
+++ Source/cmLocalVisualStudio7Generator.cxx
@@ -862,6 +862,14 @@ void cmLocalVisualStudio7Generator::WriteConfiguration(std::ostream& fout,
       }
     }
   fout << "/>\n";  // end of <Tool Name=VCCLCompilerTool
+  if(gg->IsMasmEnabled() && !this->FortranProject)
+    {
+    fout <<
+      "\t\t\t<Tool\n"
+      "\t\t\t\tName=\"MASM\"\n"
+      "\t\t\t/>\n"
+      ;
+    }
   tool = "VCCustomBuildTool";
   if(this->FortranProject)
     {
@@ -1700,6 +1708,7 @@ bool cmLocalVisualStudio7Generator
           {
           aCompilerTool = "VFFortranCompilerTool";
           }
+        std::string const& lang = (*sf)->GetLanguage();
         std::string ext = (*sf)->GetExtension();
         ext = cmSystemTools::LowerCase(ext);
         if(ext == "idl")
@@ -1726,6 +1735,11 @@ bool cmLocalVisualStudio7Generator
             {
             aCompilerTool = "VFCustomBuildTool";
             }
+          }
+        if (gg->IsMasmEnabled() && !this->FortranProject &&
+            lang == "ASM_MASM")
+          {
+          aCompilerTool = "MASM";
           }
         for(std::map<std::string, cmLVS7GFileConfig>::const_iterator
               fci = fcinfo.FileConfigMap.begin();
@@ -2095,6 +2109,16 @@ cmLocalVisualStudio7Generator::WriteProjectStart(std::ostream& fout,
        << "\t<Platforms>\n"
        << "\t\t<Platform\n\t\t\tName=\"" << gg->GetPlatformName() << "\"/>\n"
        << "\t</Platforms>\n";
+  if(gg->IsMasmEnabled())
+    {
+    fout <<
+      "\t<ToolFiles>\n"
+      "\t\t<DefaultToolFile\n"
+      "\t\t\tFileName=\"masm.rules\"\n"
+      "\t\t/>\n"
+      "\t</ToolFiles>\n"
+      ;
+    }
 }
 
 
